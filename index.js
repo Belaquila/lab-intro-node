e81f975ff98b5a91dccdaeb2571c97887b3d73dc
ab.belabdia@gmail.com <ahmed@MacBook-Pro-de-Ahmed.local>
2024-10-21 20:28:39 +0200

    done

--- index.js
+++ index.js
@@ -1,17 +1,57 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+      this.items.push(item);
+      this.items.sort((a,b) => a-b)
+      this.length = this.items.length
+    
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(pos >= 0 && pos< this.items.length){
+      return this.items[pos];
+    } else {
+      throw new Error("OutOfBounds");
+    }
+  }
 
-  max() {}
+  max() {
+    if(this.items.length > 0){
+      return this.items.reduce((a,b) => Math.max(a,b) , -Infinity) 
+      //return Math.max(...this.items) is also a solution that works :)
+    } else {
+      throw new Error('EmptySortedList')
+    }
+  }
 
-  min() {}
+  min() {
+    if(this.items.length > 0){
+      return this.items.reduce((a,b)=> Math.min(a,b), Infinity)
+    } else {
+      throw new Error('Empty SortedList')
+    }
+  }
 
-  sum() {}
+  sum() {
+    if(this.items.length === 0){
+      return 0
+    } else {
+      return this.items.reduce((acc,currentValue) => acc += currentValue, 0)
+    }
+  }
 
-  avg() {}
+  avg() {
+    if(this.items.length === 0){
+      throw new Error("Empty sortedList");
+      
+    } else {
+      return this.items.reduce((acc,currentValue) => acc += currentValue, 0)/this.items.length
+    }
+  }
 }
 
 module.exports = SortedList;
